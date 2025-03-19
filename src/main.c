135e8a5504b42eabc70facb2f17c7746b1d52476
Liam Lefebvre <lilefebv@student.42lyon.fr>
2025-03-19 16:19:26 +0100

    FIX: rm segfault from invalid map

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: lilefebv <lilefebv@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/17 15:31:47 by lilefebv          #+#    #+#             */
-/*   Updated: 2025/03/19 15:21:23 by lilefebv         ###   ########lyon.fr   */
+/*   Updated: 2025/03/19 16:17:05 by lilefebv         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,15 +18,22 @@ int	print_error(char *err)
 	return (0);
 }
 
+int	print_error1(char *err)
+{
+	ft_dprintf(2, RED"[Error]"NC" %s\n", err);
+	return (1);
+}
+
 int	main(int argc, char **argv)
 {
 	t_minirt	minirt;
 
 	if (argc < 2)
-		return (print_error("Missing scene file. Usage: ./miniRT <scene.rt>"));
+		return (print_error1("Missing scene file. Usage: ./miniRT <scene.rt>"));
 	if (argc > 2)
-		return (print_error("Too many arguments. Usage: ./miniRT <scene.rt>"));
-	parse_scene(&minirt, argv[1]);
+		return (print_error1("Too many arguments. Usage: ./miniRT <scene.rt>"));
+	if (parse_scene(&minirt, argv[1]) == 0)
+		return (1);
 	print_scene(&minirt.scene);
 	free_scene(&minirt.scene, NULL);
 }

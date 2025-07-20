10e35b6285e6997de40fbbd4b0ff09d6dd2a0580
Delvincourt Mathis <delvincourtmathis@gmail.com>
2025-07-20 14:36:46 +0200

    [add] add new caustic system

--- src/caustic/caustic_manager.c
+++ src/caustic/caustic_manager.c
@@ -6,7 +6,7 @@
 /*   By: madelvin <madelvin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/23 19:36:31 by madelvin          #+#    #+#             */
-/*   Updated: 2025/07/01 19:51:14 by madelvin         ###   ########.fr       */
+/*   Updated: 2025/07/20 13:42:12 by madelvin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -69,7 +69,7 @@ char	init_photon_map(t_minirt *minirt, int photons_per_light,
 		if (emit_photon_from_one_light(photons_per_light, minirt,
 			photon_array, i))
 		{
-			free(photon_array);
+			free(photon_array->data);
 			return (print_error1("Add photon to vector."));
 		}
 	}
--- src/material/material_default.c
+++ src/material/material_default.c
@@ -6,7 +6,7 @@
 /*   By: madelvin <madelvin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/08 18:27:14 by madelvin          #+#    #+#             */
-/*   Updated: 2025/07/01 18:03:59 by madelvin         ###   ########.fr       */
+/*   Updated: 2025/07/20 14:36:25 by madelvin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -67,56 +67,33 @@ width + uv.x * (minirt->scene.amb_light.skybox_t->hdr.width - 1))];
 	*data.power = multiply_scalar_fcolor(*data.power, 1 - costheta);
 }
 
-
-
-
-
-
-float   vec3_distance_sq(t_vec3 v1, t_vec3 v2)
+static float vec3_distance_sq(t_vec3 v1, t_vec3 v2)
 {
-    float   dx;
-    float   dy;
-    float   dz;
+    float dx = v1.x - v2.x; float dy = v1.y - v2.y;
+    float dz = v1.z - v2.z;
 
-    dx = v1.x - v2.x;
-    dy = v1.y - v2.y;
-    dz = v1.z - v2.z;
-    
     return (dx * dx + dy * dy + dz * dz);
 }
 
-
-
-
-
-
-
-
-
-
-
-
-// Structure pour stocker un photon trouvé et sa distance (au carré)
-typedef struct s_found_photon {
+typedef struct s_found_photon
+{
     t_photon* photon;
-    float       dist_sq; // On stocke la distance au carré pour éviter les sqrt()
+    float dist_sq;
 } t_found_photon;
 
-// Structure pour gérer la recherche et ses résultats
-typedef struct s_search_data {
-    const t_vec3* target_point;   // Le point autour duquel on cherche
-    t_found_photon* found_list;     // La liste des N meilleurs photons trouvés
-    int             max_photons;    // N, le nombre de photons à trouver
-    int             photons_found;  // Le nombre de photons actuellement dans la liste
-    float           max_dist_sq;    // La distance (au carré) du photon le plus éloigné
+typedef struct s_search_data
+{
+    t_vec3* target_point;
+    t_found_photon* found_list;
+    int max_photons;
+    int photons_found;
+    float max_dist_sq;
 } t_search_data;
 
-
-
-static void insert_into_found_list(t_search_data* data, t_photon* photon, float dist_sq)
+static void insert_into_found_list(t_search_data* data, t_photon* photon,
+    float dist_sq)
 {
     int i;
-
     if (data->photons_found < data->max_photons)
     {
         i = data->photons_found - 1;
@@ -128,8 +105,7 @@ static void insert_into_found_list(t_search_data* data, t_photon* photon, float 
         data->found_list[i + 1] = (t_found_photon){photon, dist_sq};
         data->photons_found++;
         data->max_dist_sq = data->found_list[data->photons_found - 1].dist_sq;
-    }
-    else if (dist_sq < data->max_dist_sq)
+    } else if (dist_sq < data->max_dist_sq)
     {
         i = data->max_photons - 2;
         while (i >= 0 && data->found_list[i].dist_sq > dist_sq)
@@ -142,124 +118,55 @@ static void insert_into_found_list(t_search_data* data, t_photon* photon, float 
     }
 }
 
-static void search_recursive(t_kd_node* node, t_search_data* data)
-{
-    float       dist_sq_to_node;
-    float       dist_to_plane;
-    t_kd_node* near_child;
-    t_kd_node* far_child;
-
+static void search_recursive(t_kd_node* node, t_search_data* data) {
     if (!node)
         return;
-
-    dist_sq_to_node = vec3_distance_sq(node->photon->position, *data->target_point);
+    float dist_sq_to_node = vec3_distance_sq(node->photon->position, *data->target_point);
     insert_into_found_list(data, node->photon, dist_sq_to_node);
-
-    dist_to_plane = get_axis_value(*data->target_point, node->axis) - get_axis_value(node->photon->position, node->axis);
-    
-    if (dist_to_plane < 0) {
-        near_child = node->left;
-        far_child = node->right;
-    } else {
-        near_child = node->right;
-        far_child = node->left;
-    }
-
+    float dist_to_plane = get_axis_value(*data->target_point, node->axis) - get_axis_value(node->photon->position, node->axis);
+    t_kd_node* near_child = (dist_to_plane < 0) ? node->left : node->right;
+    t_kd_node* far_child = (dist_to_plane < 0) ? node->right : node->left;
     search_recursive(near_child, data);
-
     if ((dist_to_plane * dist_to_plane) < data->max_dist_sq)
-    {
         search_recursive(far_child, data);
-    }
 }
 
-int find_n_nearest_photons(t_kd_tree *kdtree, t_vec3 point,
-    int num_photons_to_find, t_photon** nearest_photons, float* search_radius_sq)
+t_fcolor estimate_caustics_physically_based(t_kd_tree *caustic_kdtree, t_vec3 point, int k_nearest)
 {
-    t_found_photon  found_list[num_photons_to_find];
+    t_found_photon  *found_list;
     t_search_data   search_data;
+    t_fcolor        accumulated_flux;
+    float           area;
+    int             i;
 
-    if (!kdtree || !kdtree->root)
-    {
-        *search_radius_sq = 0.0f;
-        return (0); // Retourne 0 si pas de photons
+    if (!caustic_kdtree || !caustic_kdtree->root || k_nearest <= 0)
+        return ((t_fcolor){0, 0, 0});
+
+    found_list = malloc(sizeof(t_found_photon) * k_nearest);
+    if (!found_list)
+        return ((t_fcolor){0, 0, 0});
+
+    search_data = (t_search_data){&point, found_list, k_nearest, 0, 1e30f};
+    search_recursive(caustic_kdtree->root, &search_data);
+
+    if (search_data.photons_found == 0) {
+        free(found_list);
+        return ((t_fcolor){0, 0, 0});
     }
 
-    // Initialisation de la structure de recherche
-    search_data.target_point = &point;
-    search_data.found_list = found_list;
-    search_data.max_photons = num_photons_to_find;
-    search_data.photons_found = 0;
-    search_data.max_dist_sq = 1e30f; 
-
-    // Lancement de la recherche récursive
-    search_recursive(kdtree->root, &search_data);
-
-    // Remplissage du tableau de sortie
-    for (int i = 0; i < search_data.photons_found; i++) {
-        nearest_photons[i] = search_data.found_list[i].photon;
-    }
-    
-    if (search_data.photons_found > 0) {
-        *search_radius_sq = search_data.max_dist_sq;
-    } else {
-        *search_radius_sq = 0.0f;
-    }
-    
-    // CORRECTION: On retourne le nombre de photons qui ont été trouvés.
-    return (search_data.photons_found);
-}
-
-#define NUM_CAUSTIC_PHOTONS 50
-#define MAX_CAUSTIC_RADIUS 0.05f
-
-t_fcolor    estimate_caustics_from_map(t_kd_tree *caustic_kdtree, t_vec3 point)
-{
-    t_photon    *nearest_photons[NUM_CAUSTIC_PHOTONS];
-    float       search_radius_sq;
-    float       search_radius;
-    t_fcolor    accumulated_power;
-    float       area;
-    int         i;
-    int         photons_found;
-
-    if (!caustic_kdtree || !caustic_kdtree->root)
-        return ((t_fcolor){0.0f, 0.0f, 0.0f});
-
-    photons_found = find_n_nearest_photons(caustic_kdtree, point,
-            NUM_CAUSTIC_PHOTONS, nearest_photons, &search_radius_sq);
-
-    if (photons_found == 0)
-        return ((t_fcolor){0.0f, 0.0f, 0.0f});
-
-    search_radius = sqrtf(search_radius_sq);
-
-    // CORRECTION : On vérifie si le rayon de recherche est trop grand.
-    // Si c'est le cas, cela signifie que les photons sont trop dispersés
-    // pour former un caustic net. On ignore alors leur contribution.
-    if (search_radius > MAX_CAUSTIC_RADIUS)
-        return ((t_fcolor){0.0f, 0.0f, 0.0f});
-
-    accumulated_power = (t_fcolor){0.0f, 0.0f, 0.0f};
+    accumulated_flux = (t_fcolor){0, 0, 0};
     i = 0;
-    while (i < photons_found)
-    {
-        accumulated_power = add_fcolor(accumulated_power, nearest_photons[i]->power);
+    while (i < search_data.photons_found) {
+        accumulated_flux = add_fcolor(accumulated_flux, found_list[i].photon->power);
         i++;
     }
 
-    // On utilise le rayon réel de la recherche pour la normalisation
-    area = M_PI * search_radius_sq;
-    if (area > 1e-6f)
-    {
-        accumulated_power.r /= area;
-        accumulated_power.g /= area;
-        accumulated_power.b /= area;
-    }
-
-    return (accumulated_power);
+    area = M_PI * search_data.max_dist_sq;
+    free(found_list);
+    if (area > 1e-9f)
+        return (multiply_scalar_fcolor(accumulated_flux, 1.0f / area));
+    return ((t_fcolor){0, 0, 0});
 }
-
 
 inline void default_mat(t_minirt *minirt, t_ray *ray, t_hit_record *hit_record,
     t_ray_data data)
@@ -268,55 +175,23 @@ inline void default_mat(t_minirt *minirt, t_ray *ray, t_hit_record *hit_record,
     t_fcolor    caustics_lighting;
     t_fcolor    total_lighting;
 
-    // --- 1. ÉCLAIRAGE DIRECT (Votre code existant, inchangé) ---
     direct_lighting = compute_light_v2(hit_record, minirt);
-
-    // --- 2. NOUVELLE ÉTAPE : AJOUT DES CAUSTICS ---
-    // On estime la contribution des caustics en interrogeant la carte de photons dédiée.
-    // On ne fait cela que si une carte de caustics a été construite.
-    if (1) // Supposons que vous ayez un pointeur vers cet arbre
-    {
-        caustics_lighting = estimate_caustics_from_map(&minirt->scene.photon_map,
-            hit_record->point);
-    }
+    if (minirt->scene.photon_map.root)
+        caustics_lighting = estimate_caustics_physically_based(&minirt->scene.photon_map,
+            hit_record->point, 150);
     else
-    {
         caustics_lighting = (t_fcolor){0.0, 0.0, 0.0};
-    }
-
-    // --- 3. On combine les deux sources de lumière ---
     total_lighting = add_fcolor(direct_lighting, caustics_lighting);
-
-    // On ajoute cette contribution lumineuse à l'accumulation finale.
-    *data.accumulation = add_fcolor(
-            *data.accumulation,
-            multiply_fcolor(
-                multiply_fcolor(
-                    hit_record->color, // La couleur de l'objet (albedo)
-                    *data.power      // L'énergie actuelle du rayon
-                ),
-                total_lighting     // La lumière directe + les caustics
-            )
-        );
-
-    // --- 4. ÉCLAIRAGE INDIRECT (Votre code existant, inchangé) ---
-    // On continue le trajet du rayon pour calculer le reste de l'éclairage
-    // indirect via votre path tracer et votre importance sampling.
-    *data.power = multiply_fcolor(
-            *data.power,
-            hit_record->color
-            );
+    *data.accumulation = add_fcolor(*data.accumulation,
+        multiply_fcolor(multiply_fcolor(hit_record->color, *data.power), total_lighting));
+    *data.power = multiply_fcolor(*data.power, hit_record->color);
     if (minirt->scene.amb_light.pdf_joint)
         importance_sampling(minirt, ray, hit_record, data);
     ray->dir = cos_weighted_sample_hemishphere(&hit_record->normal);
 }
 
-inline char	default_mat_photon(t_hit_record *hit_record, t_fcolor *power)
+inline char default_mat_photon(t_hit_record *hit_record, t_fcolor *power)
 {
-	*power = multiply_fcolor(
-			*power,
-			hit_record->color
-			);
-	return (1);
+    *power = multiply_fcolor(*power, hit_record->color);
+    return (1);
 }
-

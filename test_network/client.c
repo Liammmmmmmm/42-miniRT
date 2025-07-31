ba8c882b9649cca72fd907eacd939b5fe450e80e
Liam Lefebvre <lilefebv@student.42lyon.fr>
2025-07-31 09:34:28 +0200

    server that can ping a client to connect it, and client with active and passive mode that switch automatically

--- test_network/client.c
+++ test_network/client.c
@@ -2,22 +2,191 @@
 #include <netinet/in.h>
 #include <unistd.h>
 #include <arpa/inet.h>
+#include <stdio.h>
+#include <string.h>
+#include <fcntl.h>
+#include "libft.h"
+#include <errno.h>
+#include <sys/select.h>
 
-int main() {
-	int sockfd = socket(AF_INET, SOCK_STREAM, 0);
-	
+#define PASSIVE_PORT 14242
+
+int connect_with_timeout(int sockfd, const struct sockaddr *addr, socklen_t addrlen, int timeout_sec);
+
+int passive_mode(int *sockfd) {
+	// Crée un socket en écoute
+	int passive_fd = socket(AF_INET, SOCK_STREAM, 0);
+	if (passive_fd < 0) {
+		perror("passive socket failed");
+		return -1;
+	}
+
+	struct sockaddr_in passive_addr = {
+		.sin_family = AF_INET,
+		.sin_port = htons(PASSIVE_PORT),
+		.sin_addr.s_addr = INADDR_ANY
+	};
+
+	int reuse = 1;
+	setsockopt(passive_fd, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof(reuse));
+
+	if (bind(passive_fd, (struct sockaddr*)&passive_addr, sizeof(passive_addr)) < 0)
+	{
+		perror("bind passive failed");
+		close(passive_fd);
+		return (-1);
+	}
+
+	if (listen(passive_fd, 1) < 0)
+	{
+		perror("listen passive failed");
+		close(passive_fd);
+		return (-1);
+	}
+
+	printf("En attente passive sur le port %d...\n", PASSIVE_PORT);
+
+	struct sockaddr_in server_addr;
+	socklen_t addr_len = sizeof(server_addr);
+	*sockfd = accept(passive_fd, (struct sockaddr*)&server_addr, &addr_len);
+	close(passive_fd);
+
+	if (*sockfd < 0)
+	{
+		perror("accept passive failed");
+		return (-1);
+	}
+
+	char server_ip[INET_ADDRSTRLEN];
+	inet_ntop(AF_INET, &server_addr.sin_addr, server_ip, INET_ADDRSTRLEN);
+	printf("Connexion reçue du serveur %s\n", server_ip);
+
+	return 0;
+}
+
+int active_mode(int *sockfd, char *address, char *port) {
+	*sockfd = socket(AF_INET, SOCK_STREAM, 0);
 	struct sockaddr_in server_addr = {
 		.sin_family = AF_INET,
-		.sin_port = htons(8080),
-		.sin_addr.s_addr = inet_addr("127.0.0.1")
+		.sin_port = htons(ft_atoi(port)),
+		.sin_addr.s_addr = inet_addr(address)
 	};
+
+	if (connect_with_timeout(*sockfd, (struct sockaddr*)&server_addr, sizeof(server_addr), 5) < 0) {
+		close(*sockfd);
+		return -1;
+	}
+	return 0;
+}
+
+int is_connection_alive(int sockfd) {
+	char buf[1];
+	ssize_t ret = recv(sockfd, buf, 1, MSG_PEEK | MSG_DONTWAIT);
 	
-	connect(sockfd, (struct sockaddr*)&server_addr, sizeof(server_addr));
-	send(sockfd, "GET / HTTP/1.1\r\n\r\n", 18, 0);
-	
-	char response[1024];
-	recv(sockfd, response, sizeof(response), 0);
-	printf("%s\n", response);
-	
+	if (ret == 0) {
+		return 0;  // Connexion fermée proprement
+	} else if (ret == -1) {
+		if (errno == EAGAIN || errno == EWOULDBLOCK) {
+			return 1;  // Connexion toujours active (pas de données à lire)
+		}
+		return 0;  // Erreur = connexion morte
+	}
+	return 1;  // Données disponibles = connexion active
+}
+
+int main(int argc, char **argv) {
+	int sockfd;
+
+	if (argc != 3) {
+		printf("Usage: %s <ip> <port>\n", argv[0]);
+		printf("Aucun serveur fournis, basculement en mode passif...\n");
+		if (passive_mode(&sockfd) < 0) {
+			printf("Échec du mode passif\n");
+			return 1;
+		}
+	}
+	else
+	{
+		if (active_mode(&sockfd, argv[1], argv[2]) < 0)
+		{
+			printf("Serveur introuvable, basculement en mode passif...\n");
+			if (passive_mode(&sockfd) < 0) {
+				printf("Échec du mode passif\n");
+				return 1;
+			}
+		}
+	}
+
+	// Configurer le socket pour détecter les déconnexions
+	int keepalive = 1;
+	setsockopt(sockfd, SOL_SOCKET, SO_KEEPALIVE, &keepalive, sizeof(keepalive));
+
+	while (1) {
+		// Vérifier régulièrement la connexion
+		if (!is_connection_alive(sockfd)) {
+			printf("Serveur déconnecté\n");
+			if (passive_mode(&sockfd) < 0) {
+				printf("Échec du mode passif\n");
+				break ;
+			}
+			else
+				continue ;
+		}
+
+		// Gestion de l'entrée utilisateur avec timeout
+		fd_set readfds;
+		FD_ZERO(&readfds);
+		FD_SET(STDIN_FILENO, &readfds);
+		FD_SET(sockfd, &readfds);
+
+		struct timeval select_timeout = {.tv_sec = 1, .tv_usec = 0};
+		
+		int activity = select(sockfd + 1, &readfds, NULL, NULL, &select_timeout);
+		
+		if (activity < 0) {
+			perror("select error");
+			break;
+		}
+
+		// Vérifier l'entrée utilisateur
+		if (FD_ISSET(STDIN_FILENO, &readfds)) {
+			char *line = get_next_line(STDIN_FILENO);
+			if (!line || ft_strcmp("exit\n", line) == 0) {
+				free(line);
+				break;
+			}
+			
+			if (send(sockfd, line, ft_strlen(line), 0) < 0) {
+				free(line);
+				if (passive_mode(&sockfd) < 0) {
+					printf("Échec du mode passif\n");
+					break ;
+				}
+				else
+					continue ;
+			}
+			free(line);
+		}
+
+		// Vérifier les données du serveur
+		if (FD_ISSET(sockfd, &readfds)) {
+			char response[1024];
+			ssize_t bytes = recv(sockfd, response, sizeof(response), 0);
+			
+			if (bytes <= 0) {
+				if (passive_mode(&sockfd) < 0) {
+					printf("Échec du mode passif\n");
+					break ;
+				}
+				else
+					continue ;
+			}
+			
+			response[bytes] = '\0';
+			printf("%s", response);
+		}
+	}
+
 	close(sockfd);
+	return 0;
 }
--- test_network/server.c
+++ test_network/server.c
@@ -2,27 +2,212 @@
 #include <netinet/in.h>
 #include <unistd.h>
 #include <arpa/inet.h>
+#include <signal.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <pthread.h>
+#include "libft.h"
+
+#ifndef BUFFER_SIZE
+	#define BUFFER_SIZE 1024
+#endif
+#define MAX_CLIENTS 1000
+
+int connect_with_timeout(int sockfd, const struct sockaddr *addr, socklen_t addrlen, int timeout_sec);
+
+int g_server_fd = -1;
+
+typedef struct {
+	int client_fd;
+	struct sockaddr_in client_addr;
+} client_data_t;
+
+void handle_sigint(int sig) {
+	printf("\nArrêt du serveur...\n");
+	shutdown(g_server_fd, SHUT_RDWR);
+	close(g_server_fd);
+	g_server_fd = -1;
+	exit(130);
+}
+
+void *handle_client(void *arg)
+{
+	client_data_t* data = (client_data_t*)arg;
+	int client_fd = data->client_fd;
+	char client_ip[INET_ADDRSTRLEN];
+	
+	inet_ntop(AF_INET, &(data->client_addr.sin_addr), client_ip, INET_ADDRSTRLEN);
+	printf("Thread: Nouveau client %s\n", client_ip);
+
+	char buffer[BUFFER_SIZE];
+	ssize_t bytes_received;
+	
+	while ((bytes_received = recv(client_fd, buffer, BUFFER_SIZE, 0)) > 0
+		&& g_server_fd != -1)
+	{
+		buffer[bytes_received] = '\0';
+		printf("Reçu de %s: %s\n", client_ip, buffer);
+		send(client_fd, "Ok bro j'ai recu\0", 18, 0);
+	}
+
+	if (bytes_received == 0) {
+		printf("Client %s déconnecté\n", client_ip);
+	} else {
+		perror("recv error");
+	}
+
+	close(client_fd);
+	free(data);
+	return NULL;
+}
+
+int connect_to_passive_client(const char *client_ip)
+{
+	int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+	struct sockaddr_in client_addr = {
+		.sin_family = AF_INET,
+		.sin_port = htons(14242),
+		.sin_addr.s_addr = inet_addr(client_ip)
+	};
+	
+	if (connect_with_timeout(sockfd, (struct sockaddr*)&client_addr, sizeof(client_addr), 3) < 0) {
+		close(sockfd);
+		return -1;
+	}
+	return sockfd;
+}
+
+void	replace_breakline_zero(char *line)
+{
+	while (*line)
+	{
+		if (*line == '\n')
+			*line = '\0';
+		line++;
+	}
+}
+
+void *cli_thread_routine(void *arg)
+{
+	(void)arg;
+	while (g_server_fd != -1)
+	{
+		char *line = get_next_line(0);
+		if (ft_strcmp(line, "exit\n") == 0)
+		{
+			printf("\nArrêt du serveur...\n");
+			shutdown(g_server_fd, SHUT_RDWR);
+			close(g_server_fd);
+			g_server_fd = -1;
+			exit(130);
+			break ;
+		}
+		else if (ft_strncmp(line, "link ", 5) == 0)
+		{
+			if (ft_strlen(line) < 8)
+			{
+				printf("Invalid client address\n");
+				free(line);
+				continue ;
+			}
+			replace_breakline_zero(line);
+			printf("Try connect to address : %s\n", line + 5);
+			int client_id = connect_to_passive_client(line + 5);
+			if (client_id == -1)
+			{
+				printf("Invalid client address\n");
+				free(line);
+				continue ;
+			}
+
+			client_data_t *client_data = malloc(sizeof(client_data_t));
+			if (!client_data)
+			{
+				printf("Malloc error\n");
+				free(line);
+				continue ;
+			}
+		
+			client_data->client_fd = client_id;
+			client_data->client_addr = (struct sockaddr_in){
+				.sin_family = AF_INET,
+				.sin_port = htons(14242),
+				.sin_addr.s_addr = inet_addr(line + 5)
+			};
+
+			pthread_t thread_id;
+			if (pthread_create(&thread_id, NULL, handle_client, (void*)client_data) != 0) {
+				perror("pthread_create failed");
+				close(client_data->client_fd);
+				free(client_data);
+				continue;
+			}
+			pthread_detach(thread_id);
+
+		}
+		free(line);
+	}
+	return NULL;
+}
 
 int main() {
-	int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+	signal(SIGINT, handle_sigint);
+
+	g_server_fd = socket(AF_INET, SOCK_STREAM, 0);
+	if (g_server_fd < 0) {
+		perror("socket failed");
+		exit(EXIT_FAILURE);
+	}
+
+	int reuse = 1;
+	setsockopt(g_server_fd, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof(reuse));
 
 	struct sockaddr_in addr = {
 		.sin_family = AF_INET,
-		.sin_port = htons(8080),
+		.sin_port = htons(4242),
 		.sin_addr.s_addr = INADDR_ANY
 	};
 
-	bind(sockfd, (struct sockaddr*)&addr, sizeof(addr));
-	listen(sockfd, 5);
-
-	while (1) {
-		struct sockaddr_in client_addr;
-		socklen_t client_len = sizeof(client_addr);
-		int client_fd = accept(sockfd, (struct sockaddr*)&client_addr, &client_len);
-		
-		char buffer[1024];
-		recv(client_fd, buffer, sizeof(buffer), 0);
-		send(client_fd, "HTTP/1.1 200 OK\r\n\r\nHello", 20, 0);
-		close(client_fd);
+	if (bind(g_server_fd, (struct sockaddr*)&addr, sizeof(addr)) < 0) {
+		perror("bind failed");
+		exit(EXIT_FAILURE);
 	}
+
+	if (listen(g_server_fd, MAX_CLIENTS) < 0) {
+		perror("listen failed");
+		exit(EXIT_FAILURE);
+	}
+
+	printf("Serveur en écoute sur le port 4242...\n");
+
+	pthread_t cli_thread;
+	if (pthread_create(&cli_thread, NULL, cli_thread_routine, NULL) != 0) {
+		perror("pthread_create failed, no cli");
+	}
+
+	while (g_server_fd != -1) {
+		client_data_t* client_data = malloc(sizeof(client_data_t));
+		socklen_t client_len = sizeof(client_data->client_addr);
+		
+		client_data->client_fd = accept(g_server_fd, 
+			(struct sockaddr*)&client_data->client_addr, 
+			&client_len);
+		
+		if (client_data->client_fd < 0) {
+			perror("accept failed");
+			free(client_data);
+			continue;
+		}
+
+		pthread_t thread_id;
+		if (pthread_create(&thread_id, NULL, handle_client, (void*)client_data) != 0) {
+			perror("pthread_create failed");
+			close(client_data->client_fd);
+			free(client_data);
+			continue;
+		}
+		pthread_detach(thread_id);
+	}
+
+	return 0;
 }
